a6ae3008e39c99f672cdc991914126c0a30f30f6
ZachyDev <mosetizachary001@gmail.com>
2020-04-10 02:12:04 +0300

    Gradr Challenges 1-3

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,10 @@ module.exports = {
   },
   rules: {
     "comma-dangle": ["error", "never"],
-    "no-param-reassign": ["error", { "props": false }]
+    "no-param-reassign": ["error", { "props": false }],
+    "linebreak-style": "off",
+    "no-restricted-properties": "off",
+    "max-len": [1,170,3],
+    "no-console": "off"
   },
 };
