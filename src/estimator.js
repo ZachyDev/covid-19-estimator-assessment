c0c02ba88bca949e41f6101605305a3582da9df8
ZachyDev <mosetizachary001@gmail.com>
2020-04-10 15:51:55 +0300

    Gradr covid-estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,120 +1,54 @@
-const inputData = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
-// ****** GRADR CHALLENGE 1 START *********
-const covid19ImpactEstimator = (data) => {
-  // output data
+const estimateImpact = (currentlyInfected, data) => {
+  let days;
+  const time = data.timeToElapse;
+  if (data.periodType === 'days') {
+    days = time;
+  }
+  if (data.periodType === 'weeks') {
+    days = time * 7;
+  }
+  if (data.periodType === 'months') {
+    days = time * 30;
+  }
+
+  const { region, totalHospitalBeds } = data;
+  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = region;
+
+  const factor = Math.floor(days / 3);
+  const infectionsByRequestedTime = currentlyInfected * (2 ** factor);
+  const severeCasesByRequestedTime = 0.15 * infectionsByRequestedTime;
+
+  const beds = (0.35 * totalHospitalBeds) - severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime = Math.floor(beds);
+
+  const casesForICUByRequestedTime = Math.floor(0.05 * infectionsByRequestedTime);
+  const casesForVentilatorsByRequestedTime = Math.floor(0.02 * infectionsByRequestedTime);
+
+  const economyLose = infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * days;
+  const dollarsInFlight = economyLose;
+
   const outputData = {
-    data: { inputData },
-    // impact estimation
-    impact: {
-      currentlyInfected: Object(data.reportedCases * 10),
-      infectionsAsByRequestedTime: () => {
-        // chech the periodType
-        if (data.periodType === 'days') {
-          const power = Math.floor(data.timeToElapse / 3);
-          return Object(outputData.impact.currentlyInfected * Math.pow(2, power));
-        } if (data.periodType === 'weeks') {
-          const days = (data.timeToElapse * 7);
-          const power = Math.floor(days / 3);
-          return Object(outputData.impact.currentlyInfected * Math.pow(2, power));
-        } if (data.periodType === 'months') {
-          const days = (data.timeToElapse * 30);
-          const power = Math.floor(days / 3);
-          return Object(outputData.impact.currentlyInfected * Math.pow(2, power));
-        }
-      }
-    },
-    // severe impact estimation
-    severeImpact: {
-      currentlyInfected: Object(data.reportedCases * 50),
-      infectionsAsByRequestedTime: () => {
-        // chech the periodType
-        if (data.periodType === 'days') {
-          const power = Math.floor(data.timeToElapse / 3);
-          return Object(outputData.severeImpact.currentlyInfected * Math.pow(2, power));
-        } if (data.periodType === 'weeks') {
-          const days = (data.timeToElapse * 7);
-          const power = Math.floor(days / 3);
-          return Object(outputData.severeImpact.currentlyInfected * Math.pow(2, power));
-        } if (data.periodType === 'months') {
-          const days = (data.timeToElapse * 30);
-          const power = Math.floor(days / 3);
-          return Object(outputData.severeImpact.currentlyInfected * Math.pow(2, power));
-        }
-      }
-    }, // **** GRADR CHALLENGE 1 END *****
-
-    // ******** GRADR CHALLENGE 2 START **********
-
-    severeCasesByRequestedTime: {
-      // severeCasesByRequestedTime for impact estimation as for the requested time
-      hospitalBedsByRequestedTimeForImpact: () => {
-        const severePositive = 0.15 * (outputData.impact.infectionsAsByRequestedTime());
-        const { totalHospitalBeds } = data;
-        const bedsOccupied = Math.floor(0.65 * totalHospitalBeds);
-        const bedsAvailable = totalHospitalBeds - bedsOccupied;
-        const bedStatus = bedsAvailable - severePositive;
-      },
-      // severeCasesByRequestedTime for Severe impact estimation as for the requested time
-      hospitalBedsByRequestedTimeForSevereImpact: () => {
-        const severePositive = 0.15 * (outputData.severeImpact.infectionsAsByRequestedTime());
-        const { totalHospitalBeds } = data;
-        const bedsOccupied = Math.floor(0.65 * totalHospitalBeds);
-        const bedsAvailable = totalHospitalBeds - bedsOccupied;
-        const bedStatus = bedsAvailable - severePositive;
-      }
-      // ******* GRADR CHALLENGE 2 END **************
-
-    },
-
-    // *********** GRADR CHALLENGE 3 START ***************
-    casesForICUByRequestedTime: {
-      severePositiveForICUAsPerImpact: () => {
-        const severeCasesThatRequireICU = 0.05 * (outputData.impact.infectionsAsByRequestedTime());
-        return severeCasesThatRequireICU;
-      },
-      severePositiveForICUAsPerSevereImpact: () => {
-        const severeCasesThatRequireICU = 0.05 * (outputData.severeImpact.infectionsAsByRequestedTime());
-        return severeCasesThatRequireICU;
-      }
-    },
-    casesForVentilatorsByRequestedTime: {
-      severePositiveForVentilatorsAsPerImpact: () => {
-        const severeCasesThatRequireVentilator = 0.02 * (outputData.impact.infectionsAsByRequestedTime());
-        return Math.floor(severeCasesThatRequireVentilator);
-      },
-      severePositiveForVentilatorsAsPerSevereImpact: () => {
-        const severeCasesThatRequireVentilator = 0.02 * (outputData.severeImpact.infectionsAsByRequestedTime());
-        return Math.floor(severeCasesThatRequireVentilator);
-      }
-    },
-    dollarsInFlight: {
-      dollarsInFlightLoseForImpact: () => {
-        const economyLose = (outputData.impact.infectionsAsByRequestedTime()) * (data.region.avgDailyIncomeInUSD) * 30 * (data.region.avgDailyIncomePopulation);
-        return economyLose;
-      },
-      dollarsInFlightLoseForSevereImpact: () => {
-        const economyLose = (outputData.severeImpact.infectionsAsByRequestedTime()) * (data.region.avgDailyIncomeInUSD) * 30 * (data.region.avgDailyIncomePopulation);
-        return economyLose;
-      }
-    }
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   };
-  console.log(typeof (outputData.impact.currentlyInfected));
   return outputData;
 };
-covid19ImpactEstimator(inputData);
-// module.exports = covid19ImpactEstimator;
 
+const covid19ImpactEstimator = (data) => {
+  const currentlyInfected = data.reportedCases * 10;
+  const currentlyInfectedSevere = data.reportedCases * 50;
+  const outputData = {
+    data,
+    impact: estimateImpact(currentlyInfected, data),
+    severeImpact: estimateImpact(currentlyInfectedSevere, data)
+  };
+  return outputData;
+};
 
-export default covid19ImpactEstimator;
+module.exports = covid19ImpactEstimator;
+// export default covid19ImpactEstimator;
